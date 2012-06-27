7774cde9a73ba4dbe9217dfd783f16055a9f2e4f
moriyasu0410 <moriyasu0410@gmail.com>
2012-06-27 16:19:42 +0900

    modify p => pk

--- app/model/SampleRequest.scala
+++ app/model/SampleRequest.scala
@@ -1,29 +1,38 @@
 package model
 import play.mvc.Scope.Params
-import validator.SampleRequestValidator
+import _root_.validators.SampleRequestValidator
+import _root_.daos.kvses.SampleRequestDao
+import models.JsonDeSerializer
+import models.JsonSerializer
+
 case class SampleRequest(
-  val p: String = "",
+  val pk: Option[String] = None,
   val a: Option[String],
   val b: Option[String],
-  val c: Option[String]) extends SampleRequestValidator{
-}  
-object SampleRequest {
+  val c: Option[String]) extends SampleRequestValidator with SampleRequestDao with JsonSerializer{
+  def prefix = "sample_request_"
+  override def key = pk
+}
+
+
+
+object SampleRequest extends JsonDeSerializer[SampleRequest]{
   def bind(param: Params): SampleRequest = {
     val b = param.get("b")
     if (b != null) {
       new SampleRequest(
         a = Some(param.get("a")),
         b = Some(param.get("b")),
-        p = param.get("p"),
+        pk = Some(param.get("pk")),
         c = Some(param.get("c")))
 
     } else {
       new SampleRequest(
         a = Some(param.get("a")),
         b = None,
-        p = param.get("p"),
+        pk = Some(param.get("pk")),
         c = Some(param.get("c")))
-      
+
     }
   }
 }
